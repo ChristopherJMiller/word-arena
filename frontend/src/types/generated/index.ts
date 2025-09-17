333ae74521323812cb7137ec034ef92b597e4d52
Chris Miller <chrmiller@microsoft.com>
2025-09-17 09:36:14 -0400

    fix index ts

---
+++ frontend/src/types/generated/index.ts
@@ -0,0 +1,18 @@
+// Auto-generated TypeScript types from Rust
+// This file is regenerated when running: npm run types:generate
+
+export type { ClientMessage } from "./ClientMessage";
+export type { ConnectionError } from "./ConnectionError";
+export type { ConnectionInfo } from "./ConnectionInfo";
+export type { GameError } from "./GameError";
+export type { GamePhase } from "./GamePhase";
+export type { GameState } from "./GameState";
+export type { GameStatus } from "./GameStatus";
+export type { GuessResult } from "./GuessResult";
+export type { LetterResult } from "./LetterResult";
+export type { LetterStatus } from "./LetterStatus";
+export type { PersonalGuess } from "./PersonalGuess";
+export type { Player } from "./Player";
+export type { SafeGameState } from "./SafeGameState";
+export type { ServerMessage } from "./ServerMessage";
+export type { User } from "./User";
